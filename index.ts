707bc9067d364f33a26791dd3af13edc021bd282
Joanaescar <joanaa-esteves@hotmail.com>
2022-12-10 00:55:53 +0000

    Installed cors dependency

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response, NextFunction } from 'express';
 const app: Express = express();
 
 import { connect } from 'mongoose';
+import cors from 'cors';
 
 import dotenv from 'dotenv';
 dotenv.config();
@@ -17,6 +18,7 @@ connect('mongodb://localhost:27017/kind-karma')
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
 
 app.use('/users', UserController);
